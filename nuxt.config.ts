e803cf2f7d5fe8573981631c20d4ce9b079b3a60
Samuel Antwi <samuelantwidev@gmail.com>
2022-06-11 00:09:00 +0100

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+import { defineNuxtConfig } from "nuxt"
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  module: ["@nuxt/content", "@vueuse/nuxt"],
+  buildModule: [],
+
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+  },
+
+  css: ["~/assets/css/tailwind.css"],
+  build: {
+    postcss: {
+      postcssOptions: require("./postcss.config.js"),
+    },
+    transpile: ["@heroicons/vue", "@headlessui/vue"],
+  },
+})
